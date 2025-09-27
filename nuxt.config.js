a6e1de587108f3916b959357f13172a5f29ab7cf
MayurDange15 <68801382+MayurDange15@users.noreply.github.com>
2025-09-27 17:05:54 +0530

    Done

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 export default {
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Bio-FAIR 2k25 | National Level Technical Event',
